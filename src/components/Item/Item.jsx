c4eea52a6bae5457b6b59e307f33233e6a9ff9a5
anikaaurony <aurony111@gmail.com>
2023-04-04 02:30:50 +0600

    fake data commit

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Item = (props) => {
+    console.log(props.item)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Item;
---
+++ src/components/Items/Items.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Item from '../Item/Item';
+import "./Items.css"
+
+const Items = () => {
+    const [items, setItems] = useState([]);
+    useEffect(()=>{
+        fetch('fakedata.json')
+        .then(res=>res.json())
+        .then(data=>setItems(data))
+    },[])
+    // console.log(items)
+    return (
+        <div className='itemsContainer'>
+            <div className='productContainer'>
+                <h4>Items Container</h4>
+                {
+                    items.map(item => <Item
+                    key = {item.id}
+                    item = {item}
+                    ></Item>)
+                }
+                
+            </div>
+            <div>Items cart</div>
+        </div>
+    );
+};
+
+export default Items;
