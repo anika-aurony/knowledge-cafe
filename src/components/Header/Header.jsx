578ae93f04fd31a30955cd0f10fc65ee73c8096b
anikaaurony <aurony111@gmail.com>
2023-04-03 21:59:48 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2>Knowledge Cafe</h2>
+            <img src="https://img.freepik.com/free-photo/pretty-smiling-joyfully-female-with-fair-hair-dressed-casually-looking-with-satisfaction_176420-15187.jpg?w=900&t=st=1680536330~exp=1680536930~hmac=a1b6fe0bc27fea91c7c0bb2bf67aec7ceeb72c0725af711d0097f7a1c3578501" alt="" srcset="" />
+        </div>
+    );
+};
+
+export default Header;
